dac927c127fab724ec13c6d21c92cd674a064a87
Torres-Brayan <130689664+Torres-Brayan@users.noreply.github.com>
2023-05-21 11:13:44 -0500

    latest version

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-
-function game1() {
-    Name1 = (window.prompt("Enter your name"));
-    Guess1 = Number(window.prompt("Guess A Number 1 - 10"));
-    let ranNum = 5//Math.floor(Math.random()*10) + 1;
-    guessCount = [];
-    Tries1 = 1;
-
-    while (Guess1 != ranNum) {
-        Tries1 ++;
-        guessCount.push(Guess1);
-        if (Guess1 > ranNum) {
-        alert(` Sorry ${Name1} You Guessed to High, Try again.`);
-        Guess1 = Number(window.prompt("Guess A Number 1 - 10"));
-        } else {
-
-        alert(` Sorry ${Name1} You Guessed to low, Try again.`);
-        Guess1 = Number(window.prompt("Guess A Number 1 - 10"));
-        }
-    }
-    if (localStorage.getItem(Name1) > Tries1) {
-        localStorage.setItem(Name1, Tries1);
-        alert(`Good Guess ${Name1} you are correct! It only took you ${Tries1} tries. Your previous attempts were ${guessCount}, You beat your previous attempt.`);
-        if (confirm("Would you like to play again?") === true) {
-            game1();
-        }
-    } else if (localStorage.getItem(Name1) < Tries1) {
-        localStorage.setItem(Name1, Tries1);
-        alert(`Good Guess ${Name1} you are correct! It only took you ${Tries1} tries. Your previous attempts were ${guessCount}, You did better your last attempt.`);
-        if (confirm("Would you like to play again?") === true) {
-            game1();
+function guessGame(){
+       let number= 5;
+       let guess = "";
+       let amountOfGuesses = 0;
+       let exactGuesses = [];
+        let name = "";
+ 
+    name = prompt("Input your name here: ");
+        do {
+            guess = prompt("Guess a number: "); 
+            if (number < guess) {
+                alert(`Sorry, ${name} you've guessed too high! Keep guessing!`);
+            } else if (number > guess) {
+                alert(`Sorry, ${name} you've guessed too low! Keep guessing!`);
+            } 
+            amountOfGuesses ++;
+            exactGuesses.push(guess);
+        } while (guess != number);
+        if(!firstgame) {
+        if (localStorage.getItem(name[amountOfGuesses]) > amountOfGuesses) {
+            alert(`Good Job! ${name} number of guesses were ${amountOfGuesses} and your exact numbers used were ${exactGuesses} You beat your previous attempt.`);
+            localStorage.setItem(name, amountOfGuesses);
+            playAgain();
+        } else if (localStorage.getItem(name[amountOfGuesses]) < amountOfGuesses) {
+            alert(`Good Job! ${name} number of guesses were ${amountOfGuesses} and your exact numbers used were ${exactGuesses} You did better your previous attempt.`);
+            localStorage.setItem(name, amountOfGuesses);
+            playAgain();
+            console.log(localStorage.getItem(name));
+        } else if (localStorage.getItem(name[amountOfGuesses]) == amountOfGuesses) {
+            alert(`Good Job! ${name} number of guesses were ${amountOfGuesses} and your exact numbers used were ${exactGuesses} You tied your previous attempt.`);
+            localStorage.setItem(name, amountOfGuesses);
+            playAgain();
         }
     } else {
-    localStorage.setItem(Name1, Tries1);
-    alert(`Good Guess ${Name1} you are correct! It only took you ${Tries1} tries. Your previous attempts were ${guessCount}`);
-    if (confirm("Would you like to play again?") === true) {
-        game1();
+        alert(`Good Job! ${name} number of guesses were ${amountOfGuesses} and your exact numbers used were ${exactGuesses}.`);
+        localStorage.setItem(name, amountOfGuesses);
+        playAgain();
+
     }
 }
+guessGame()
+
+function playAgain(){
+    if (confirm("Would you like to play again? ")){
+        guessGame();
+    } 
 }
-game1();
